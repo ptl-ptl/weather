bf3adadd90715705194cc6398e495c1ef56ca2be
ptl-ptl <155249971+ptl-ptl@users.noreply.github.com>
2024-01-11 11:32:33 +0100

    Added the search functionality using the API

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-app-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "67b3fe199e6a5df80a44a5o53t65b217";
+  let urlApi = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(urlApi).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
@@ -7,3 +22,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Ghent");
